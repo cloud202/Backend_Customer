fd521a178c5d7b2a1bd71439ad89e2a59a0c9289
jayeshcloud202 <jayesh.m@cloud202.com>
2023-10-03 12:34:29 +0530

    feat(api): implemented a new endpoint for updating task. This commit introduces a new API endpoint that allows users to update a particular task. closes #cq-182

--- validators/project/phaseValidator.js
+++ validators/project/phaseValidator.js
@@ -1,9 +1,14 @@
 const Joi = require('joi');
+const JoiObjectId = require('joi-objectid');
+Joi.objectId = JoiObjectId(Joi);
 
 const phaseSchema = Joi.object({
+    customerId: Joi.string().required(),
+    projectOid: Joi.objectId().required(),
+    phaseOid: Joi.objectId().required(),
     name: Joi.string(),
     description: Joi.string(),
     scope: Joi.string(),
-},{unknown:false});
+}, { unknown: false });
 
 module.exports = phaseSchema;
